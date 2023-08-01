369735169daba1280e0f6f40534cd45d469a2046
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-08-01 10:11:33 +0530

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,11 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
+
+//writing the variables
 
 let firstName = "Matilda";
 console.log(firstName);
@@ -14,3 +17,37 @@ let $function = 27;
 
 let person = "jonas";
 let PI = 3.14; // write constants  in uppercase
+
+*/
+
+/*
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "raghav");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1998;
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1998;
+// birthYear = 1999;   cant change  cost
+
+// const job;   thisi willl  show error
