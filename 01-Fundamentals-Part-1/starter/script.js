661f0a1f48f1cf4fa39af807eeacf748d49328d3
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-08-01 09:34:43 +0530

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,15 @@
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
