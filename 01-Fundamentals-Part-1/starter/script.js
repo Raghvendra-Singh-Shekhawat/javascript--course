57a0c1feb5b9aa45c5945ad27a405635b6e70043
Raghvendra <88329661+Raghvendra-Singh-Shekhawat@users.noreply.github.com>
2023-09-18 09:12:00 +0530

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -195,3 +195,17 @@ const age = 18;
 if (age === 18) console.log("you just became an adult");
 
 if (age == 18) console.log("you just became an adult");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
